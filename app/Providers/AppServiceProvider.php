6dfdb405c789b89fa6a8adc88cdf86a931826f53
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-04-04 16:55:02 +0530

    string length changes

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -26,6 +26,6 @@ class AppServiceProvider extends ServiceProvider
     public function boot()
     {
         Paginator::useBootstrap();
-        Schema::defaultStringLength(255);
+        Schema::defaultStringLength(191);
     }
 } 
