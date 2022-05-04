1e547d41d28b0b26ca00f5bbefa16b0df6fe8d29
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-05-04 14:17:01 +0530

    Batches DB structering

---
+++ app/Models/Batches.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Batches extends Model
+{
+    use HasFactory;
+    protected $fillable = [
+        'brand',
+        'supplier',
+        'packing_size',
+        'quantity',
+        'batch',
+        'serial',
+        'po_number',
+        'statutory_body',
+        'euc_material',
+        'require_bulk_volume_tracking',
+        'require_outlife_tracking',
+        'outlife',
+        'storage_area',
+        'housing_type',
+        'housing',
+        'owner_one',
+        'owner_two',
+        'dept',
+        'access',
+        'date_in',
+        'date_of_expiry',
+        'coc_coa_mill_cert',
+        'iqs_status',
+        'iqs_result',
+        'sds',
+        'cas',
+        'fm1202',
+        'project_name',
+        'material_product_type',
+        'date_of_manufacture',
+        'date_of_shipment',
+        'cost_per_unit',
+        'remarks',
+        'extended_expiry',
+        'extended_qc_status',
+        'extended_qc_result',
+        'disposal_certificate',
+        'used_for_td/expt_only',
+    ];
+}
---
+++ database/migrations/2022_05_04_063958_create_batches_table.php
@@ -0,0 +1,70 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateBatchesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('batches', function (Blueprint $table) {
+            $table->id();
+            $table->integer('material_product_id');
+            $table->string('brand')->nullable();
+            $table->string('supplier')->nullable();
+            $table->string('packing_size')->nullable();
+            $table->string('quantity')->nullable();
+            $table->string('batch')->nullable();
+            $table->string('serial')->nullable();
+            $table->string('po_number')->nullable();
+            $table->string('statutory_body')->nullable();
+            $table->string('euc_material')->nullable();
+            $table->string('require_bulk_volume_tracking')->nullable();
+            $table->string('require_outlife_tracking')->nullable();
+            $table->string('outlife')->nullable();
+            $table->string('storage_area')->nullable();
+            $table->string('housing_type')->nullable();
+            $table->string('housing')->nullable();
+            $table->string('owner_one')->nullable();
+            $table->string('owner_two')->nullable();
+            $table->string('dept')->nullable();
+            $table->string('access')->nullable();
+            $table->string('date_in')->nullable();
+            $table->string('date_of_expiry')->nullable();
+            $table->string('coc_coa_mill_cert')->nullable();
+            $table->string('iqc_status')->nullable();
+            $table->string('iqc_result')->nullable();
+            $table->string('sds')->nullable();
+            $table->string('cas')->nullable();
+            $table->string('fm_1202')->nullable();
+            $table->string('project_name')->nullable();
+            $table->string('material_product_type')->nullable();
+            $table->string('date_of_manufacture')->nullable();
+            $table->string('date_of_shipment')->nullable();
+            $table->string('cost_per_unit')->nullable();
+            $table->text('remarks')->nullable();
+            $table->string('extended_expiry')->nullable();
+            $table->string('extended_qc_status')->nullable();
+            $table->string('extended_qc_result')->nullable();
+            $table->string('disposal_certificate')->nullable();
+            $table->string('used_for_td_expt_only')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('batches');
+    }
+}
