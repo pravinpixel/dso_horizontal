2ec65c089eddaf9562edd1041f651c8b0cf0ca8e
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-06-16 15:31:27 +0530

    Theam Customizing

--- resources/views/crm/partials/table-th-column.blade.php
+++ resources/views/crm/partials/table-th-column.blade.php
@@ -1,20 +1,21 @@
 <div ng-show="on_item_description" class="sticky-left">
     <div class="box position-relative h-100 th">
         Item Description
-        <i ng-click="sort_by('id', 'asc')" class="bi bi-arrow-up  position-absolute top-0 right-0 cur_ponit"></i>
-        <i ng-click="sort_by('id', 'desc')" class="bi bi-arrow-down  position-absolute bottom-0 right-0 cur_ponit"></i>
+        <i ng-click="sort_by('id', 'asc')" class="bi bi-caret-up-fill btn-sort top-0"></i>
+        <i ng-click="sort_by('id', 'desc')"
+            class="bi bi-caret-down-fill btn-sort bottom-0"></i>
     </div>
 </div>
-@foreach ($tableAllColumns as $column) 
+@foreach ($tableAllColumns as $column)
     <div ng-if="on_{{ $column['name'] }}" class="position-relative box th">
-        <i ng-click="sort_by('{{ $column['name'] }}', 'asc')" class="bi bi-arrow-up  position-absolute top-0 right-0 cur_ponit"></i>
-        <i ng-click="sort_by('{{ $column['name'] }}', 'desc')" class="bi bi-arrow-down  position-absolute bottom-0 right-0 cur_ponit"></i>
-        <span class="text-capitalize"> 
-            {{ str_replace('_', " ", $column['name']) }}
-        </span>
+        <i ng-click="sort_by('{{ $column['name'] }}', 'asc')"
+            class="bi bi-caret-up-fill btn-sort top-0"></i>
+        <i ng-click="sort_by('{{ $column['name'] }}', 'desc')"
+            class="bi bi-caret-down-fill btn-sort bottom-0"></i>
+        {{ ucfirst(str_replace('_', ' ', $column['name'])) }}
     </div>
 @endforeach
-<div class="box th border-start {{ $page_name !== 'PRINT_BARCODE_LABEL'  ? "box-sm" : null}}">
+<div class="box th border-start {{ $page_name !== 'PRINT_BARCODE_LABEL' ? 'box-sm' : null }}">
     Actions
-    {{ $page_name === 'PRINT_BARCODE_LABEL'  ? "/ Qty to print" : null}}
+    {{ $page_name === 'PRINT_BARCODE_LABEL' ? '/ Qty to print' : null }}
 </div>
