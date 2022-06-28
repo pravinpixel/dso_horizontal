0ecb31fecd1b77ea63bbf987d595e17edc0e3ea0
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-06-28 14:39:40 +0530

    Db Designing

---
+++ database/migrations/2022_06_28_143234_create_table_orders_table.php
@@ -0,0 +1,32 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateTableOrdersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('table_orders', function (Blueprint $table) {
+            $table->id();
+            $table->longText('column')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('table_orders');
+    }
+}
