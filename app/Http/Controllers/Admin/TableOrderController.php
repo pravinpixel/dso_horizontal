68bb553157173c96f586cf888afcfba20f06ff34
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-06-28 14:39:56 +0530

    Table Order Render View

--- app/Http/Controllers/Admin/TableOrderController.php
+++ app/Http/Controllers/Admin/TableOrderController.php
@@ -3,12 +3,15 @@
 namespace App\Http\Controllers\Admin;
 
 use App\Http\Controllers\Controller;
+use App\Models\tableOrder;
+use App\Models\User;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Schema;
 
 class TableOrderController extends Controller
 {
     public function index(Request $request)
-    {
-        return view('masters.table-order.index');
+    { 
+        return view('masters.table-order.index', compact('table_orders'));
     }
 }
