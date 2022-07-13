ba6a9818cbec29aa625dc52d81099516210b01ac
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-07-13 13:19:00 +0530

    TransferBatch Duplicate Row issue

--- app/Http/Controllers/TransferBatchController.php
+++ app/Http/Controllers/TransferBatchController.php
@@ -10,11 +10,16 @@ class TransferBatchController extends Controller
 {
     public function transfer(Request $request)
     {
-        $current_batch                  =   Batches::find($request->id);
-        $created_batch                  =   $current_batch->replicate();
-        $created_batch->created_at      =   Carbon::now();
-        $created_batch->quantity        =   $request->quantity;
-        $created_batch->barcode_number  =   generateBarcode(MaterialProducts::find($request->material_product_id)->category_selection);
+        $current_batch                 = Batches::find($request->id);
+        $created_batch                 = $current_batch->replicate();
+        $created_batch->created_at     = Carbon::now();
+        $created_batch->quantity       = $request->quantity;
+        $created_batch->barcode_number = generateBarcode(MaterialProducts::find($request->material_product_id)->category_selection);
+        $created_batch->storage_area   = $request->storage_area;
+        $created_batch->housing_type   = $request->housing_type;
+        $created_batch->housing        = $request->housing;
+        $created_batch->owner_one      = $request->owner_one;
+        $created_batch->owner_two      = $request->owner_two;
         $created_batch->save();
 
         $current_batch->update([
