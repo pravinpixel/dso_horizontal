0110826c35ddb1345d6dcfd9a37c63dab7e7914b
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-07-25 14:47:31 +0530

    Print Label remove draft entry

--- resources/views/crm/partials/data-table.blade.php
+++ resources/views/crm/partials/data-table.blade.php
@@ -15,11 +15,21 @@
                         {{-- ======= Matrial Product Data  ====== --}}
                     </div>
                     <div class="custom-table collapse show batch-table" id="row_@{{ index+1 }}">
-                        <div class="custom-table-row " ng-repeat="batch in row.batches" ng-class="batch.is_draft == 1 ? 'drafted' : 'non-drafted'">
-                            {{-- ======= Matrial Product Batches Data  ====== --}}
-                                {!! $batch_table_td_columns !!} 
-                            {{-- ======= Matrial Product Batches Data  ====== --}}
-                        </div>
+                        {{-- ======= Matrial Product Batches Data  ====== --}}
+                            @switch($page_name)
+                                @case('MATERIAL_SEARCH_OR_ADD')
+                                    <div class="custom-table-row " ng-repeat="batch in row.batches" ng-class="batch.is_draft == 1 ? 'drafted' : 'non-drafted'">
+                                            {!! $batch_table_td_columns !!} 
+                                    </div>
+                                @break
+                                @case('PRINT_BARCODE_LABEL')
+                                    <div class="custom-table-row " ng-repeat="batch in row.batches" ng-if="batch.is_draft != 1">
+                                        {!! $batch_table_td_columns !!} 
+                                    </div>
+                                @break
+                                @default
+                            @endswitch 
+                        {{-- ======= Matrial Product Batches Data  ====== --}}
                     </div>
                 </div>
             </div> 
