0a8a140cc3c17059ddd2c3031cb9f889a01c609b
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-09-13 17:31:31 +0530

    Early Disposal Action Changes

--- resources/views/crm/partials/batch-table-td-column.blade.php
+++ resources/views/crm/partials/batch-table-td-column.blade.php
@@ -1,6 +1,6 @@
-<div class="box box-lg" ng-show="on_item_description"> </div>   
- 
-@foreach ($tableAllColumns as $column) 
+<div class="box box-lg" ng-show="on_item_description"></div>
+
+@foreach ($tableAllColumns as $column)
     @if ($column['name'] != 'item_description' && $column['name'] != 'owner_one' && $column['name'] != 'batch' && $column['name'] != 'material_product_id')
         <div ng-if="on_{{ $column['name'] }}" class="box text-center"> 
             @switch($column['name'])
@@ -120,24 +120,6 @@
                         <i class="bi bi-trash2 me-1"></i>
                       To Dispose /  <br>  Used for TD / Expt Project 
                     </a>
-                    <button class="dropdown-item text-secondary"  ng-click="view_batch_details(row, batch)"><i class="bi bi-eye"></i> View batch details</button>
-                    <button class="dropdown-item text-secondary"  ng-click="duplicateThisBatch(batch.id)"><i class="bi bi-back me-1"></i>Duplicate batch</button>
-                    <button class="dropdown-item text-secondary"  ng-click="editOrDuplicate('edit',row.id, batch.id)"><i class="bi bi-pencil-square me-1"></i>Edit batch</button>
-                    <button class="dropdown-item text-secondary" ng-disabled="batch.is_draft == 1"  ng-click="Transfers(batch.id ,  row.quantity)"><i class="bi bi-arrows-move me-1"></i>Transfer</button>
-        
-                    {{--  ==== REPACK OUTLIFE ====  --}}
-                    {{-- ng-disabled="batch.require_outlife_tracking ==  1 || batch.is_draft == 1" --}}
-                        <button  class="dropdown-item text-secondary" ng-click="RepackTransfers('view',batch , row)">
-                            <i class="bi bi-box-seam me-1"></i> Repack/Transfer 
-                        </button>
-                    {{--  ==== REPACK OUTLIFE ====  --}}
-        
-                    {{--  ==== REPACK OUTLIFE ====  --}}
-                        <button class="dropdown-item text-secondary"  ng-click="RepackOutlife(batch, row.unit_of_measure)">
-                            <i class="bi bi-box2-fill me-1"></i> Repack/outlife
-                        </button> 
-                    {{--  ==== REPACK OUTLIFE ====  --}}
-        
                     <button class="dropdown-item text-secondary" ng-disabled="batch.is_draft == 1" ng-click="printBatchLabel(batch.id)" ><i class="bi bi-upc-scan me-1"></i>Print Barcode/Label</button>
                     <button class="dropdown-item text-danger" ng-click="delete_batch_material_product(batch.id)"><i class="bi bi-trash3-fill me-1"></i> Delete batch</button>  
                 </div>
