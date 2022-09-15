c3f8b3d92b198919481765dbc348bb37abdeee74
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-09-15 15:03:39 +0530

    Cart Table Setup

---
+++ app/Repositories/ProductCartRepository.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Repositories\Interfaces\ProductCartRepositoryInterface;
+
+class ProductCartRepository implements ProductCartRepositoryInterface
+{
+    public function __construct()
+    {
+        //
+    }
+}
