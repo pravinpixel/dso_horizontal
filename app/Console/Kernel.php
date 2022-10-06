1ea0664f7fe00435792e63539a7608e5d409ffcc
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-10-06 13:21:42 +0530

    Auto Draw IN Function

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -2,11 +2,15 @@
 
 namespace App\Console;
 
+use App\Console\Commands\DrawIN;
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
 class Kernel extends ConsoleKernel
 {
+    protected $commands = [
+        DrawIN::class
+    ];
     /**
      * Define the application's command schedule.
      *
@@ -15,7 +19,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule)
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('drawIn:cron')->everyMinute();
     }
 
     /**
