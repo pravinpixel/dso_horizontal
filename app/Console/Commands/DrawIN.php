5b6f2442437d5bf2f1d6772da0bdfdc933668f80
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-10-07 10:04:49 +0530

    Repack Outlife Auto DrawIN 8.5 hours function

--- app/Console/Commands/DrawIN.php
+++ app/Console/Commands/DrawIN.php
@@ -45,43 +45,50 @@ class DrawIN extends Command
     public function handle()
     {
         $Batches = Batches::with("RepackOutlife")->get();
-
         foreach($Batches as $batch) {
             foreach($batch->RepackOutlife as $outlife) {
                 if($outlife->draw_in == 0 && $outlife->draw_out == 1) { //$outlife->draw_in == 0 && $outlife->draw_out == 1
-                    if($batch->unit_packing_value != 0) {
-                        $batch->RepackOutlife()->create([
-                            'input_repack_amount' => $outlife['repack_size']
-                        ]);
-                    }
-                    $draw_out_date_strToTime = strtotime($outlife->created_at);
-                    $draw_in_date_strToTime  = strtotime(now());
-                    $remaining_days_seconds  = abs($draw_out_date_strToTime-$draw_in_date_strToTime);
-
-                    if($batch->outlife_seconds === null) {
-                        $updated_outlife_seconds    =  (int) $batch->outlife * 86400 - (int) $remaining_days_seconds;
-                    } else {
-                        $updated_outlife_seconds    =  (int) $batch->outlife_seconds - (int) $remaining_days_seconds;
-                    }
-
-                    $current_outlife_expiry  =  CarbonImmutable::now()->add($updated_outlife_seconds, 'second')->toDateTimeString();
+                    $time1     = new DateTime($outlife->created_at);
+                    $time2     = new DateTime();
+                    $time_diff = $time1->diff($time2);
                     
-                    $batch->update([
-                        'outlife_seconds'   => $updated_outlife_seconds,
-                        'updated_outlife'   => dateDifferStr(new DateTime("@0"),new DateTime("@$updated_outlife_seconds"))
-                    ]); 
+                    if($time_diff->h.".".$time_diff->i >= env('AUTO_DRAW_TIMING')) {
 
-                    $outlife->update([
-                        'draw_in'                 => 1,
-                        'draw_out'                => 1,
-                        'draw_out_time_stamp'     => now()->format("F jS, Y, g:i:s"),
-                        'remain_days'             => dateDifferStr(new DateTime($outlife->created_at), new DateTime()),
-                        'remaining_days_seconds'  => $remaining_days_seconds,
-                        'current_date_time'       => Carbon::now()->toDateTimeLocalString(),
-                        'updated_outlife'         => dateDifferStr(new DateTime("@0"),new DateTime("@$updated_outlife_seconds")),
-                        'updated_outlife_seconds' => $updated_outlife_seconds,
-                        'current_outlife_expiry'  => $current_outlife_expiry,
-                    ]); 
+                        if($batch->unit_packing_value != 0) {
+                            $batch->RepackOutlife()->create([
+                                'input_repack_amount' => $outlife['repack_size']
+                            ]);
+                        }
+
+                        $draw_out_date_strToTime = strtotime($outlife->created_at);
+                        $draw_in_date_strToTime  = strtotime(now());
+                        $remaining_days_seconds  = abs($draw_out_date_strToTime-$draw_in_date_strToTime);
+    
+                        if($batch->outlife_seconds === null) {
+                            $updated_outlife_seconds    =  (int) $batch->outlife * 86400 - (int) $remaining_days_seconds;
+                        } else {
+                            $updated_outlife_seconds    =  (int) $batch->outlife_seconds - (int) $remaining_days_seconds;
+                        }
+    
+                        $current_outlife_expiry  =  CarbonImmutable::now()->add($updated_outlife_seconds, 'second')->toDateTimeString();
+                        
+                        $batch->update([
+                            'outlife_seconds'   => $updated_outlife_seconds,
+                            'updated_outlife'   => dateDifferStr(new DateTime("@0"),new DateTime("@$updated_outlife_seconds"))
+                        ]); 
+    
+                        $outlife->update([
+                            'draw_in'                 => 1,
+                            'draw_out'                => 1,
+                            'draw_out_time_stamp'     => now()->format("F jS, Y, g:i:s"),
+                            'remain_days'             => dateDifferStr(new DateTime($outlife->created_at), new DateTime()),
+                            'remaining_days_seconds'  => $remaining_days_seconds,
+                            'current_date_time'       => Carbon::now()->toDateTimeLocalString(),
+                            'updated_outlife'         => dateDifferStr(new DateTime("@0"),new DateTime("@$updated_outlife_seconds")),
+                            'updated_outlife_seconds' => $updated_outlife_seconds,
+                            'current_outlife_expiry'  => $current_outlife_expiry,
+                        ]); 
+                    }
                 }
             }
         }
