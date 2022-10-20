7ef4ba5e546d8869edf6b40ece6977d74c2b56f2
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-10-20 15:06:58 +0530

    Nil Value Changes

--- database/seeders/HouseTypeSeeder.php
+++ database/seeders/HouseTypeSeeder.php
@@ -24,7 +24,7 @@ class HouseTypeSeeder extends Seeder
             ['name' => "Dry Cabinet"],
             ['name' => "Freezer"],
             ['name' => "Pallet"],
-            ['name' => "nil"],
+            ['name' => "Nil"],
         ];
                 
         HouseTypes::insert($data);
