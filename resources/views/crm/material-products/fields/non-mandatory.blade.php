e1c78dedb09172dbfe9f2f7a35059d76dec2dba7
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-11-21 12:22:20 +0530

    FM1202 Check box issue

--- resources/views/crm/material-products/fields/non-mandatory.blade.php
+++ resources/views/crm/material-products/fields/non-mandatory.blade.php
@@ -27,7 +27,7 @@
             <label for="" class="col-4">FM1202 </label>
             <div class="col-8"> 
                 <div class="form-control form-control-sm {{ config(is_disable(category_type() ?? $material_product->category_selection ?? null)."fm_1202.status") == 'disabled' ? "bg-light" : '' }}">  
-                    <input type="checkbox" {{ $batch->fm_1202 == 'on' ? 'checked' : '' }} name="fm_1202" value="{{ $batch->fm_1202 ?? null }}" class="form-check-input me-2" {{ config(is_disable(category_type() ?? $material_product->category_selection ?? null)."fm_1202.status") }}>               
+                    <input type="checkbox" {{ $batch->fm_1202 == 'on' ? 'checked' : '' }} name="fm_1202" value="on" class="form-check-input me-2" {{ config(is_disable(category_type() ?? $material_product->category_selection ?? null)."fm_1202.status") }}>               
                     <small>Yes Included</small>
                 </div>
             </div>
