6d879e8afe8eab9b781ddb8fea0ae4b3b54b7879
prabhu-pixel <prabhu.k.pixel@gmail.com>
2022-12-12 19:29:28 +0530

    Code Cleaning

--- resources/views/includes/sections/top-nav-bar.blade.php
+++ resources/views/includes/sections/top-nav-bar.blade.php
@@ -2,10 +2,10 @@
     <div class="container-fluid">
         <nav class="navbar navbar-light navbar-expand-lg topnav-menu ">
             <div class="collapse navbar-collapse" id="topnav-menu-content">
-                <ul class="navbar-nav custom">                    
+                <ul class="navbar-nav custom">
                     <li class="nav-item dropdown">
                         <a href="{{ route('dashboard') }}" class="nav-link {{ Route::is('dashboard') ? "active" : "" }}"><i class="bi bi-speedometer2 me-1"></i><span>Dashboard</span></a>
-                    </li>  
+                    </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle arrow-none {{ Route::is(['material-product.edit-form-one','material-product.edit-form-two','material-product.edit-form-three']) ? "active" : "" }} {{ Route::is(['withdrawal_index','list-material-products','mandatory-form-one','mandatory-form-two','non-mandatory-form']) ? "active" : "" }}" href="#" id="topnav-dashboards" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="bi bi-briefcase me-1"></i> Material / In-house Product <div class="arrow-down"></div>
@@ -14,14 +14,14 @@
                             <a href="{{ route('withdrawal_index') }}" class="dropdown-item {{ Route::is(['withdrawal_index']) ? "active" : "" }}"><i class="bi bi-aspect-ratio me-1"></i><span>Withdrawal</span></a>
                             <a href="{{ route('list-material-products') }}" class="dropdown-item {{ Route::is(['list-material-products']) ? "active" : "" }}"><i class="bi bi-plus-circle-dotted me-1"></i><span>Search or Add</span></a>
                         </div>
-                    </li> 
+                    </li>
                     <li class="nav-item dropdown">
                         <a class="nav-link dropdown-toggle arrow-none {{ Route::is(['threshold-qty','near-expiry-expired']) ? "active" : "" }}" href="#" id="topnav-dashboards" role="button" data-bs-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
                             <i class="bi bi-bell me-1"></i> Notification <div class="arrow-down"></div>
                         </a>
                         <div class="dropdown-menu" aria-labelledby="topnav-dashboards">
                             <a href="{{ route('threshold-qty') }}" class="dropdown-item {{ Route::is(['threshold-qty']) ? "active" : "" }}"><i class="bi bi-subtract me-1"></i><span>Threshold Qty</span></a>
-                            <a href="{{ route('near-expiry-expired') }}" class="dropdown-item {{ Route::is(['near-expiry-expired']) ? "active" : "" }}"><i class="bi bi-list-ul me-1"></i><span>Near Expiry/Expired </span></a> 
+                            <a href="{{ route('near-expiry-expired') }}" class="dropdown-item {{ Route::is(['near-expiry-expired']) ? "active" : "" }}"><i class="bi bi-list-ul me-1"></i><span>Near Expiry/Expired </span></a>
                         </div>
                     </li>
                     <li class="nav-item dropdown">
@@ -31,16 +31,16 @@
                         <a href="{{ route('extend-expiry') }}" class="nav-link {{ Route::is('extend-expiry') ? 'active' : '' }}"><i class="bi bi-arrow-up-right-square me-1"></i>Extended Expiry</a>
                     </li>
                     <li class="nav-item dropdown">
-                        <a href="{{ route('reports') }}" class="nav-link {{ Route::is(['reports','reports.utilisation_cart','reports.export_cart','reports.history','reports.disposed_items']) ? 'active' : '' }}"><i class="bi bi-file-earmark-bar-graph me-1"></i>Report  </a>
-                    </li> 
+                        <a href="{{ route('reports') }}" class="nav-link {{ Route::is(['reports','reports.disposed_items','reports.expired_material','reports.security','reports.deduct_track_outlife','reports.deduct_track_usage','reports.material_in_house_pdt_history']) ? 'active' : '' }}"><i class="bi bi-file-earmark-bar-graph me-1"></i>Report  </a>
+                    </li>
                     <li class="nav-item dropdown">
                         <a href="{{ route('barcode.listing') }}" class="nav-link {{ Route::is('barcode.listing') ? 'active' : '' }}"><i class="bi bi-upc-scan me-1"></i>Print Label </a>
                     </li>
                     <li class="nav-item dropdown">
                         <a href="{{ route('reconciliation') }}" class="nav-link {{ Route::is(['reconciliation','view-reconciliation']) ? "active" : "" }}"><i class="bi bi-arrow-repeat me-1"></i>Reconciliation</a>
-                    </li> 
+                    </li>
                 </ul>
             </div>
         </nav>
     </div>
-</div> 
+</div>
--- routes/web.php
+++ routes/web.php
@@ -101,8 +101,6 @@ Route::middleware(['auth_users'])->group(function () {
     //  ==================================
     Route::get('/add-material-products-other-form', [MaterialProductsController::class, 'other_form_index'])->name('other-form');
     Route::post('/add-material-products-other-form', [MaterialProductsController::class, 'other_form_store'])->name('other-form');
-
-
     // ===================== Print Label =====================
         Route::prefix('print-label')->group(function() {
             Route::get('/', [PrintBarcodeController::class, 'index'])->name('barcode.listing');
@@ -110,11 +108,8 @@ Route::middleware(['auth_users'])->group(function () {
             Route::post('/{id?}', [PrintBarcodeController::class, 'print'])->name('print.barcode');
         });
     // ===================== Print Label =====================
-
     // ==================== Repack Draw IN / OUT Flow ===============
         Route::post('/repack-batch/{batch_id}', [RepackBatchController::class, 'repack_outlife'])->name('repack_outlife');
-        // Route::get('/repack-batch/{batch_id}', [RepackBatchController::class, 'get_repack_outlife'])->name('repack_outlife');
-
         Route::post('/store-repack-batch/{batch_id}', [RepackBatchController::class, 'store_repack_outlife'])->name('store_repack_outlife');
         Route::get('/export-repack-batch/{batch_id}', [RepackBatchController::class, 'export_repack_outlife'])->name('export_repack_outlife');
 
@@ -122,8 +117,6 @@ Route::middleware(['auth_users'])->group(function () {
 
     // Word Suggestion
     Route::post('/get-suggestion', [MaterialProductsController::class, 'suggestion'])->name('suggestion');
-
-
     Route::prefix('withdrawal')->group(function() {
         Route::get('/material-are-in-house-products', [WithdrawalController::class, 'index'])->name('withdrawal_index');
         Route::post('/withdrawal-direct-deduct', [WithdrawalController::class, 'direct_deduct'])->name('direct-deduct');
@@ -131,13 +124,11 @@ Route::middleware(['auth_users'])->group(function () {
         Route::post('/withdrawal-deduct-track-outlife', [WithdrawalController::class, 'deduct_track_outlife'])->name('deduct-track-outlife');
     });
     Route::delete('/withdrawal/withdrawal-deduct-track-usage', [WithdrawalController::class, 'deduct_track_usage_clear'])->name('deduct-track-usage');
-
     Route::get('/decrease-quantity/{id}', [WithdrawalController::class, 'decrease_quantity'])->name('decrease-quantity');
     Route::get('/get-withdrawal-data/{type}', [WithdrawalController::class, 'get_withdrawal_data'])->name('get-withdrawal_data');
     Route::get('/delete-withdraw-cart/{id}', [WithdrawalController::class, 'delete_withdraw_cart'])->name('delete-withdraw_cart');
     Route::get('/get-withdraw-cart-count', [WithdrawalController::class, 'withdraw_cart_count'])->name('withdraw-cart-count');
     Route::get('/get-withdrawal-batches/{barcode?}', [WithdrawalController::class, 'withdrawal_indexing'])->name('withdrawal-indexing');
-
     Route::prefix('notification')->group(function() {
         Route::get('threshold-qty', [NotificationController::class,'threshold_index'])->name('threshold-qty');
         Route::post('change-read-status/{batch_id?}', [NotificationController::class,'change_read_status'])->name('change-read-status');
@@ -145,57 +136,38 @@ Route::middleware(['auth_users'])->group(function () {
     });
     Route::get('NotificationCount',[NotificationController::class,'notification_count']);
     Route::get('/near-expiry-expired-ajax/{type?}',[NotificationController::class,'near_expiry_expired_ajax'])->name('near_expiry_expired_ajax');
-
     Route::prefix('extend-expiry')->group(function() {
         Route::get('extend-expiry/{id?}', [ExtendExpiryController::class,'index'])->name('extend-expiry');
         Route::post('/extend-expiry/{id?}', [ExtendExpiryController::class,'update'])->name('update.extend-expiry');
     });
     Route::get('/get-extend-expiry/{id?}', [ExtendExpiryController::class,'show'])->name('view.extend-expiry');
-
     Route::prefix('early-disposal')->group(function() {
         Route::get('disposal/{id?}', [EarlyDisposalController::class,'index'])->name('disposal');
         Route::post('/disposal/{id?}', [EarlyDisposalController::class,'disposal_update'])->name('update.disposal');
     });
     Route::get('/get-disposal-expiry/{id?}', [EarlyDisposalController::class,'show'])->name('view.disposal');
-
     Route::prefix('reports')->group(function() {
         Route::get('/', [ReportsController::class,'material_in_house_pdt_history'])->name('reports');
-
         Route::get('disposed-items', [ReportsController::class,'disposed_items'])->name('reports.disposed_items');
         Route::get('expired-material', [ReportsController::class,'expired_material'])->name('reports.expired_material');
         Route::get('security', [ReportsController::class,'security'])->name('reports.security');
-
         Route::get('deduct-track-outlife', [ReportsController::class,'deduct_track_outlife'])->name('reports.deduct_track_outlife');
         Route::get('deduct-track-outlife/download/{id}', [ReportsController::class,'deduct_track_outlife_download'])->name('reports.deduct_track_outlife_download');
-
         Route::get('deduct-track-usage', [ReportsController::class,'deduct_track_usage'])->name('reports.deduct_track_usage');
         Route::post('deduct-track-usage-download', [ReportsController::class,'deduct_track_usage_download'])->name('reports.deduct_track_usage_download');
-
         Route::get('material-in-house-pdt-history', [ReportsController::class,'material_in_house_pdt_history'])->name('reports.material_in_house_pdt_history');
         Route::post('material-in-house-pdt-history', [ReportsController::class,'material_in_house_pdt_history_download'])->name('reports.material_in_house_pdt_history_download');
-        // Route::get('export-cart', [ReportsController::class,'export_cart'])->name('reports.export_cart');
-        // Route::get('history', [ReportsController::class,'history'])->name('reports.history');
     });
-
     Route::get('reports/get-material-product-history/{barcode?}', [ReportsController::class,'get_material_product_history'])->name('get-material-product-history');
     Route::get('reports/check-material-product-history/{barcode?}/{check}', [ReportsController::class,'get_material_product_history']);
     Route::get('reports/export', [ReportsController::class,'export'])->name('reports.export');
     Route::post('reports/export/disposed-items', [ReportsController::class,'export_disposed_items'])->name('reports.export_disposed_items');
     Route::post('export-security', [ReportsController::class,'security_export'])->name('reports.export-security');
-
-
     Route::post('reports/export/expired-material', [ReportsController::class,'export_expired_material'])->name('reports.export_expired_material');
-
-
-
     Route::resource('/product-cart', ProductCartController::class);
     Route::get('/get-product-cart',[ ProductCartController::class,'index']);
-
     Route::get('/delete-file/{id}', [MaterialProductsController::class, 'delete_file']);
     Route::get('/delete-batch-file/{id}/{type}', [MaterialProductsController::class, 'delete_batch_file']);
-
-
-
     Route::prefix('download-files')->group(function() {
         Route::post('/{id}/{type}', [DownloadController::class,'download'])->name('download-files');
     });
