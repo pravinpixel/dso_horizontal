b25c668b9cf79e096116cd844c5684d436695d90
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-02-23 11:52:49 +0530

    favicon.ico

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,6 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <link href="https://www.dso.org.sg/media/default/theme/favicon.png" rel="shortcut icon" type="image/png" />
     <title>DSO</title>
     <meta name="csrf-token" content="{{ csrf_token() }}" />
     <meta name="app-url" content="{{ url('') }}" />
