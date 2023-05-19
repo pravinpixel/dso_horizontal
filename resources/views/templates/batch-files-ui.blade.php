43b54a3c45b7a740698206aeb83ce0f615a48120
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-05-19 16:00:04 +0530

    download type history issues

--- resources/views/templates/batch-files-ui.blade.php
+++ resources/views/templates/batch-files-ui.blade.php
@@ -3,7 +3,7 @@
         @foreach ($files as $key => $file)
             @if ($file->column_name === $type)
                 <div class="d-flex align-items-center border shadow-sm p-1 rounded me-1 mt-1">
-                    <button onclick="download('{{ $file->id }}','coc_coa_mill_cert')"
+                    <button onclick="download('{{ $file->id }}','{{ $type }}')"
                         class="badge bg-warning rounded-pill text-dark ms-1 border-0" type="button">
                         <i class="fa fa-download me-1"></i>
                         <i>{{ $file->original_name }}</i>
