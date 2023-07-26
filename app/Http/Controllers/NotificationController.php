7ff0c00ff166b067b1db5dbd578ab4c1ad0bb98b
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-07-26 18:14:40 +0530

    Threshold-qty update

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -44,41 +44,32 @@ class NotificationController extends Controller
             'Batches.StatutoryBody',
             'Batches.BatchMaterialProduct'
         ])->where(['is_read' => 0, 'is_draft' => 0,])->orderBy('updated_at')->get();
+
         $material_products =  $this->dsoRepository->renderTableData($data, [
+            "response"  => "JSON",
+            "page_name" => "THRESHOLD_QTY"
+        ]);
+        $material_products_data =  $this->dsoRepository->renderTableData($data, [
             "response"  => "JSON",
             "page_name" => "MATERIAL_SEARCH_OR_ADD"
         ]);
-        $notifications = [
-            'EXPIRY_TABLE' => [],
-            'NEAR_EXPIRY_TABLE' => [],
-            'FAILED_IQC_TABLE' => [],
-        ];
-        foreach ($material_products as $key => $value) {
-            foreach ($value['Batches'] as $index => $row) {
-                if ($row->date_of_expiry_color === 'text-danger') {
-                    $notifications['EXPIRY_TABLE'][] = $row;
-                } elseif ($row->date_of_expiry_color === 'text-warning') {
-                    $notifications['NEAR_EXPIRY_TABLE'][] = $row;
-                } elseif ($row->iqc_status == 0 && $row->quantity != 0) {
-                    $notifications['FAILED_IQC_TABLE'][] = $row;
-                }
-                if ($row['permission'] == 'READ_ONLY') {
-                    unset($material_products[$key]['Batches'][$index]);
-                }
-            }
-            if (count($value['Batches']) == 0 || $value['material_quantity_color'] == 'text-success') {
-                unset($material_products[$key]);
-            }
+        $notifications = [];
+        foreach (['EXPIRY_TABLE','NEAR_EXPIRY_TABLE','FAILED_IQC_TABLE'] as $type) {
+            $notifications[$type][] = $this->near_expiry_expired_ajax($type, true);
         }
         NEFNotification::truncate();
         foreach ($notifications as $type => $data) {
-            if (count($data) !== 0) {
-                foreach ($data as $key => $batch) {
-                    if ($batch->notification_status == 0) {
-                        NEFNotification::updateOrCreate([
-                            'batch_id' => $batch['id'],
-                            'type'     => $type,
-                        ]);
+            if (count($data)) {
+                foreach ($data as $key => $batches) {
+                    if(count($batches)) {
+                        foreach($batches as $batch) {
+                            if ($batch->notification_status == 0) {
+                                NEFNotification::updateOrCreate([
+                                    'batch_id' => $batch['id'],
+                                    'type'     => $type,
+                                ]);
+                            }
+                        }
                     }
                 }
             }
@@ -87,13 +78,16 @@ class NotificationController extends Controller
             "NEAR_EXPIRY_TABLE" => NEFNotification::where('type', 'NEAR_EXPIRY_TABLE')->get(),
             "EXPIRY_TABLE"      => NEFNotification::where('type', 'EXPIRY_TABLE')->get(),
             "FAILED_IQC_TABLE"  => NEFNotification::where('type', 'FAILED_IQC_TABLE')->get(),
-        ];
+        ]; 
         $view = view('templates.notification-modal', compact('material_products', 'notification_data'));
         return response()->json([
             'status' => 200,
             'view'   => "$view",
             'data'   => $material_products,
-            'count'  => count($material_products) + count($notification_data['NEAR_EXPIRY_TABLE']) + count($notification_data['EXPIRY_TABLE']) + count($notification_data['FAILED_IQC_TABLE']),
+            'count'  => (count($material_products)
+                + count($notification_data['NEAR_EXPIRY_TABLE'])
+                + count($notification_data['EXPIRY_TABLE'])
+                + count($notification_data['FAILED_IQC_TABLE'])),
         ]);
     }
     public function near_expiry_expired_index()
