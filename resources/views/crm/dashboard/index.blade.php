cd71d063d1d87b6ff2eb30483008f9af604df89a
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-08-07 13:55:06 +0530

    place-items-center

--- resources/views/crm/dashboard/index.blade.php
+++ resources/views/crm/dashboard/index.blade.php
@@ -3,7 +3,7 @@
     <div class="row mt-4">
         <x-has-access name="withdrawal_index">
             <div class="col-lg-3 col-md-4 mb-3">
-                <div class="h-100  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
+                <div class="h-100 d-grid place-items-center  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
                     <a href="{{ route('withdrawal_index') }}" class="card-body btn">
                         <i class="text-warning bi bi-aspect-ratio fa-2x mb-2"></i>
                         <div class="text-primary"><b>Withdrawal</b></div>
@@ -13,7 +13,7 @@
         </x-has-access>
         <x-has-access name="list-material-products">
             <div class="col-lg-3 col-md-4 mb-3">
-                <div class="h-100  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
+                <div class="h-100 d-grid place-items-center  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
                     <a href="{{ route('list-material-products') }}" class="card-body btn">
                         <i class="text-warning bi bi-plus-circle-dotted fa-2x mb-2"></i>
                         <div class="text-primary"><b> Search or Add</b></div>
@@ -23,7 +23,7 @@
         </x-has-access>
         <x-has-access name="threshold-qty">
             <div class="col-lg-3 col-md-4 mb-3">
-                <div class="h-100  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
+                <div class="h-100 d-grid place-items-center  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
                     <a href="{{ route('threshold-qty') }}" class="card-body btn">
                         <i class="text-warning bi bi-subtract fa-2x mb-2"></i>
                         <div class="text-primary"><b> Threshold Qty</b></div>
@@ -33,7 +33,7 @@
         </x-has-access>
         <x-has-access name="near-expiry-expired">
             <div class="col-lg-3 col-md-4 mb-3">
-                <div class="h-100  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
+                <div class="h-100 d-grid place-items-center  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
                     <a href="{{ route('near-expiry-expired') }}" class="card-body btn">
                         <i class="text-warning bi bi-list-ul fa-2x mb-2"></i>
                         <div class="text-primary"><b> Near Expiry / Expired / Failed IQC </b></div>
@@ -43,7 +43,7 @@
         </x-has-access>
         <x-has-access name="disposal">
             <div class="col-lg-3 col-md-4 mb-3">
-                <div class="h-100  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
+                <div class="h-100 d-grid place-items-center  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
                     <a href="{{ route('disposal') }}" class="card-body btn">
                         <i class="text-warning bi bi-trash2 fa-2x mb-2"></i>
                         <div class="text-primary"><b> Early Disposal</b></div>
@@ -53,7 +53,7 @@
         </x-has-access>
         <x-has-access name="extend-expiry">
             <div class="col-lg-3 col-md-4 mb-3">
-                <div class="h-100  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
+                <div class="h-100 d-grid place-items-center  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
                     <a href="{{ route('extend-expiry') }}" class="card-body btn">
                         <i class="text-warning bi bi-arrow-up-right-square fa-2x mb-2"></i>
                         <div class="text-primary"><b> Extended Expiry</b></div>
@@ -63,7 +63,7 @@
         </x-has-access>
         <x-has-access name="reports">
             <div class="col-lg-3 col-md-4 mb-3">
-                <div class="h-100  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
+                <div class="h-100 d-grid place-items-center  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
                     <a href="{{ route('reports') }}" class="card-body btn">
                         <i class="text-warning bi bi-file-earmark-bar-graph fa-2x mb-2"></i>
                         <div class="text-primary"><b> Report</b></div>
@@ -73,7 +73,7 @@
         </x-has-access>
         <x-has-access name="threshold-qty">
             <div class="col-lg-3 col-md-4 mb-3">
-                <div class="h-100  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
+                <div class="h-100 d-grid place-items-center  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
                     <a href="{{ route('barcode.listing') }}" class="card-body btn">
                         <i class="text-warning bi bi-upc-scan fa-2x mb-2"></i>
                         <div class="text-primary"><b> Print Label</b></div>
@@ -83,7 +83,7 @@
         </x-has-access>
         <x-has-access name="reconciliation">
             <div class="col-lg-3 col-md-4 mb-3">
-                <div class="h-100  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
+                <div class="h-100 d-grid place-items-center  shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn">
                     <a href="{{ route('reconciliation') }}" class="card-body btn">
                         <i class="text-warning bi bi-arrow-repeat fa-2x mb-2"></i>
                         <div class="text-primary"><b> Reconciliation</b></div>
