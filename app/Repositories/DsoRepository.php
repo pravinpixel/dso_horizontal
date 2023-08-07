39eafd619199585e44424c58b4671965a0293b96
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-08-07 16:48:10 +0530

    test 1

--- app/Repositories/DsoRepository.php
+++ app/Repositories/DsoRepository.php
@@ -244,7 +244,7 @@ class DsoRepository implements DsoRepositoryInterface
             if (count($material->Batches) == 0) {
                 unset($access_material_product[$material_index]);
             } else {
-                array_values($material->Batches->toArray());
+                $material->Batches = array_values($material->Batches->toArray());
             }
             if ($page_name == 'THRESHOLD_QTY') {
                 if ($material->material_quantity_color == 'text-success' || $material->material_quantity_color == 'text-dark' || $material->is_draft == 1) {
