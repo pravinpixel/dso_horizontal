df5a5068ca43407042f729190a284b6b7d81bcdd
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-08-07 14:10:31 +0530

    ui update

--- resources/views/masters/menu.blade.php
+++ resources/views/masters/menu.blade.php
@@ -4,13 +4,13 @@
         <div class="col-md-4 p-2">
             <x-has-access name="table-order.index">
                 <a class="shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn p-3 fw-bold align-items-center d-flex flex-column {{ Route::is(['table-order.index']) ? 'active' : '' }}"
-                    href="{{ route('table-order.index') }}"><i class="bi bi-table text-warning bi bi-folder fa-2x me-2"></i> Table Order</a>
+                    href="{{ route('table-order.index') }}"><i class="bi bi-table text-warning bi bi-folder fa-2x"></i> Table Order</a>
             </x-has-access>
         </div>
         <div class="col-md-4 p-2">
             <x-has-access :name="['master.item-description', 'master-settings']">
                 <a class="shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn p-3 fw-bold align-items-center d-flex flex-column {{ Route::is(['master.item-description']) ? 'active' : '' }}"
-                    href="{{ route('master.item-description') }}"><i class="bi bi-diagram-2-fill text-warning bi bi-folder fa-2x me-2"></i> Data
+                    href="{{ route('master.item-description') }}"><i class="bi bi-diagram-2-fill text-warning bi bi-folder fa-2x"></i> Data
                     center
                 </a>
         </div>
@@ -18,26 +18,26 @@
         <div class="col-md-4 p-2">
             <x-has-access :name="['user.index', 'user.create', 'user.edit']">
                 <a class="shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn p-3 fw-bold align-items-center d-flex flex-column {{ Route::is(['user.index', 'user.create', 'user.edit']) ? 'active' : '' }}"
-                    href="{{ route('user.index') }}"><i class="bi bi-person-plus-fill text-warning bi bi-folder fa-2x me-2"></i>Users</a>
+                    href="{{ route('user.index') }}"><i class="bi bi-person-plus-fill text-warning bi bi-folder fa-2x"></i>Users</a>
             </x-has-access>
         </div>
         <div class="col-md-4 p-2">
             <x-has-access :name="['role.index', 'role.create', 'role.edit']">
                 <a class="shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn p-3 fw-bold align-items-center d-flex flex-column {{ Route::is(['role.index', 'role.create', 'role.edit']) ? 'active' : '' }}"
-                    href="{{ route('role.index') }}"><i class="bi bi-person-rolodex text-warning bi bi-folder fa-2x me-2"></i>Roles</a>
+                    href="{{ route('role.index') }}"><i class="bi bi-person-rolodex text-warning bi bi-folder fa-2x"></i>Roles</a>
             </x-has-access>
         </div>
         <div class="col-md-4 p-2">
             <x-has-access :name="['help.menu.index', 'help.menu.create', 'help.menu.edit']">
                 <a class="shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn p-3 fw-bold align-items-center d-flex flex-column {{ Route::is(['help.menu.index', 'help.menu.create', 'help.menu.edit']) ? 'active' : '' }}"
-                    href="{{ route('help.menu.index') }}"><i class="bi bi-question-circle-fill text-warning bi bi-folder fa-2x me-2"></i>Help
+                    href="{{ route('help.menu.index') }}"><i class="bi bi-question-circle-fill text-warning bi bi-folder fa-2x"></i>Help
                     Menu</a>
             </x-has-access>
         </div>
         <div class="col-md-4 p-2">
             <x-has-access name="pictogram.index">
                 <a class="shadow-sm card shadow-hover border-hover animate__animated animate__fadeIn p-3 fw-bold align-items-center d-flex flex-column {{ Route::is(['pictogram.index']) ? 'active' : null }}"
-                    href="{{ route('pictogram.index') }}"><i class="bi bi-boxes text-warning bi bi-folder fa-2x me-2"></i> Pictogram</a>
+                    href="{{ route('pictogram.index') }}"><i class="bi bi-boxes text-warning bi bi-folder fa-2x"></i> Pictogram</a>
             </x-has-access>
         </div>
     </div>
