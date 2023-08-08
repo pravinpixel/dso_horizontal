cd3fda4096f7f3582cc1b10a0f58be48700e6029
prabhu-pixel <prabhu.k.pixel@gmail.com>
2023-08-08 15:12:39 +0530

    FEAT : Product history report

--- resources/views/crm/reports/templates/material-in-house-pdt-history.blade.php
+++ resources/views/crm/reports/templates/material-in-house-pdt-history.blade.php
@@ -1,21 +1,22 @@
 <table class="table table-bordered table-sm">
     <thead class="bg-primary text-white text-center">
         <tr>
-            <th style="font-weight: bold">Category Selection</th>
+            <th style="font-weight: bold">Category selection</th>
             <th style="font-weight: bold">Item description</th>
             <th style="font-weight: bold">Brand</th>
             <th style="font-weight: bold">Batch#/Serial#</th>
-            <th style="font-weight: bold">Transaction Date</th>
-            <th style="font-weight: bold">Transaction Time</th>
+            <th style="font-weight: bold">Transaction date</th>
+            <th style="font-weight: bold">Transaction time</th>
             <th style="font-weight: bold">Transacted</th>
             <th style="font-weight: bold">Module</th>
             <th style="font-weight: bold">Action Taken</th>
             <th style="font-weight: bold">Unit packing value</th>
             <th style="font-weight: bold">Quantity</th>
+            <th style="font-weight: bold">Total quantity</th>
             <th style="font-weight: bold">Storage area</th>
             <th style="font-weight: bold">Housing </th>
             <th style="font-weight: bold">Owners</th>
-            <th style="font-weight: bold">Remarks	</th>
+            <th style="font-weight: bold">Remarks</th>
             <th style="font-weight: bold">Draw status</th>
             <th style="font-weight: bold">Remaining outlife of parent </th>
         </tr>
@@ -34,6 +35,7 @@
                 <td>{{ $data->ActionTaken }}</td>
                 <td>{{ $data->UnitPackingValue }}</td>
                 <td>{{ $data->Quantity }}</td>
+                <td>{{ number_format($data->UnitPackingValue *  $data->Quantity, 3, ".", "") }}</td>
                 <td>{{ $data->StorageArea }}</td>
                 <td>{{ $data->Housing }}</td>
                 <td>{{ $data->Owners }}</td>
