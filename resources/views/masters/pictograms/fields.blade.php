40b27e5362170f3f44729c46e80b66cfc885d3aa
ajithmv <80975119+ajith-mv@users.noreply.github.com>
2024-02-20 16:40:48 +0530

    dso validation changes

--- resources/views/masters/pictograms/fields.blade.php
+++ resources/views/masters/pictograms/fields.blade.php
@@ -8,5 +8,7 @@
     <label class="col-2 text-end col-form-label">Image</label>
     <div class="col-10">
         {!! Form::file('image', ['class' => 'form-control', 'required']) !!}
+        <label style="color:red;">Please upload the image with 150 x 150 in dimension</label>
     </div>
+
 </div>
